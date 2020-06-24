a9c25b3a491fce3f2fba8c943f4acbdee8f3ed4b
tianpeirong <tianpeirong@chinalawinfo.com>
2020-06-24 17:02:26 +0800

    数据权限修改

--- yboot-plus-back-modules/yboot-base-server/src/main/java/com/yboot/base/config/security/ResourceSecurityConfig.java
+++ yboot-plus-back-modules/yboot-base-server/src/main/java/com/yboot/base/config/security/ResourceSecurityConfig.java
@@ -2,6 +2,7 @@ package com.yboot.base.config.security;
 
 import com.yboot.base.config.properties.IgnoredUrlsProperties;
 import com.yboot.base.config.security.jwt.RestAccessDeniedHandler;
+import com.yboot.base.config.security.permission.MyFilterSecurityInterceptor;
 import com.yboot.base.config.security.validate.ImageValidateFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.Expression
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -33,11 +35,15 @@ public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
     private ImageValidateFilter imageValidateFilter;
     @Autowired
     private RestAccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = http.authorizeRequests();
         // 除配置文件忽略路径其它所有请求都需经过认证和授权
-        for(String url : ignoredUrlsProperties.getUrls()){
+        for (String url : ignoredUrlsProperties.getUrls()) {
             registry.antMatchers(url).permitAll();
         }
         registry
@@ -49,7 +55,11 @@ public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
                 .and()
                 // 自定义权限拒绝处理类
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-                registry.and().
-                addFilterBefore(imageValidateFilter, UsernamePasswordAuthenticationFilter.class);
+        registry.and().
+                addFilterBefore(imageValidateFilter, UsernamePasswordAuthenticationFilter.class)
+                // 添加自定义权限过滤器
+                .addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class)
+
+        ;
     }
 }
